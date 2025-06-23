4ecb351ee0896393c7012cfac150fcc2393d3eb6
Jack Taylor <jst55@cam.ac.uk>
2025-06-23 12:33:01 +0100

    Solute.py

---
+++ Solute.py
@@ -0,0 +1,163 @@
+import numpy as np
+from scipy.linalg import solve_banded
+
+class Solute:
+    '''
+    Represents a solute in a solution.
+
+    :param name: The name of the solute
+    :param D: The diffusion coefficient of the solute in cm²s⁻¹
+    :param conc_bulk: The bulk concentration of the solute in mol dm⁻³
+    :param charge: The charge of the solute in e
+    '''
+    def __init__(
+            self,
+            name:      str,
+            D:         float,
+            conc_bulk: float = 0.0,
+            charge:    int   = 0
+            ) -> None:
+        self.name       = name
+        self.D          = D         # Diffusion coefficient in cm²s⁻¹
+        self.conc_bulk  = conc_bulk # Bulk concentration in mol dm⁻³
+        self.charge     = charge    # Charge in e
+
+
+    def initialise_concentration(
+            self,
+            npoints: int
+            ) -> None:
+        '''
+        Sets up the concentrations of the solute in the solution by creating an 
+        array of length npoints, setting everywhere to the bulk concentration.
+
+        :param npoints: The number of points in the concentration array
+        '''
+        self.conc = np.array(
+            [self.conc_bulk] * npoints,
+            dtype=float
+            )
+        self.npoints = npoints
+
+    
+    def calculate_K(
+            self,
+            dx:  float,
+            dt:  float
+            ) -> float:
+        '''
+        Calculates K = D*dt/2dx² (dimensionless)
+
+        This value is used in constructing the matrices for modelling diffusion.
+        Note that diffusion constant is in cm²s⁻¹, which is why dx is in cm.
+
+        :param dx: The spatial step size in cm
+        :param dt: The time step in seconds
+        '''
+        K = self.D * dt / (2 * dx**2)
+        return K
+    
+
+    def construct_A_banded_format(
+            self,
+            dx: float,
+            dt: float
+            ) -> np.ndarray:
+        '''
+        Constructs the banded matrix A for modelling diffusion via the Crank-
+        Nicholson method, with a reflective wall boundary condition on the left
+        and a constant concentration boundary on the right.
+
+        :return: The banded matrix A in the form accepted by the scipy linalg
+            banded solver.
+        '''
+        K = self.calculate_K(dx, dt)
+        if not hasattr(self, 'npoints'):
+            raise ValueError("Concentration not initialised. " \
+                             "Concentration must be initialised first.")
+        
+        # Initialise the banded matrix A
+        A_banded          = np.zeros((3, self.npoints))
+        # Upper diagonal all values equal to -K
+        A_banded[0, 1:  ] = -K
+        # Lower diagonal all but last value equal to -K
+        # Last value is 0 because of the constant concentration boundary
+        A_banded[2,  :-2] = -K
+        # Main diagonal, 1 + 2K equal everywhere except first and last
+        A_banded[1, 1:-1] = 1 + 2*K
+        # First row, reflective wall boundary condition
+        A_banded[1, 0   ] = 1 + K
+        # Last row, constant concentration boundary condition
+        A_banded[1, -1  ] = 1
+        return A_banded
+    
+
+    def construct_B(
+            self,
+            dx: float,
+            dt: float
+            ) -> np.ndarray:
+        '''
+        Constructs the matrix B for modelling diffusion via Crank-Nicholson
+        method, with a reflective wall boundary condition on the left and a
+        constant concentration boundary on the right. This is the matrix which
+        is applied on the future time-step side of the equation.
+
+        :return: The matrix B in full format.
+        '''
+        K = self.calculate_K(dx, dt)
+        if not hasattr(self, 'npoints'):
+            raise ValueError("Concentration not initialised. " \
+                             "Concentration must be initialised first.")
+        
+        # Initialise the matrix B
+        B = np.zeros((self.npoints, self.npoints))
+
+        # Set the non-boundary values
+        for i in range(1, self.npoints-1):
+            B[i, i-1] = K
+            B[i, i]   = 1 - 2*K
+            B[i, i+1] = K
+
+        # Reflective Wall Boundary Conditions
+        B[0, 0] = 1 - K
+        B[0, 1] = K
+        # Dirichlet Boundary Condition
+        B[-1, -1] = 1
+        return B
+    
+
+    def save_diffusion_matrices(
+            self,
+            dx: float,
+            dt: float
+            ) -> None:
+        '''
+        Construct and save the reusable matrices relevant for modelling
+        diffusion of the solute via the Crank-Nicholson technique.
+
+        :param dx: The spatial step size in cm
+        :param dt: The time step in seconds
+        '''
+        self.A_banded = self.construct_A_banded_format(dx, dt)
+        self.B        = self.construct_B(dx, dt)
+
+    
+    def diffuse(
+            self
+            ) -> None:
+        '''
+        Updates the concentration of the solute in the solution by allowing
+        diffusion to occur over one time step using the Crank-Nicholson method.
+        This method relies on the matrices A_banded and B being pre-computed
+        and stored in the solute object.
+        The time step and spatial step are set in the calculated matrices.
+
+        Effectively solves the equation A * conc_new = B * conc_old to obtain
+        the new concentrations.
+        '''
+        self.conc = solve_banded(
+            (1,1),
+            self.A_banded,
+            self.B @ self.conc
+            )
